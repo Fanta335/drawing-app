87bfacde7136ef07a64084ccf35cf2029aac7707
Fanta335 <fantayusuke5210@gmail.com>
2022-01-07 17:47:35 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Stage, Layer, Line } from "react-konva";
 
-function App() {
+const App = () => {
+  const [tool, setTool] = React.useState("pen");
+  const [size, setSize] = React.useState(5);
+  const [color, setColor] = React.useState("#000000");
+  const [lines, setLines] = React.useState([]);
+  const isDrawing = React.useRef(false);
+
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y], size, color }]);
+  };
+
+  const handleMouseMove = (e) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+      <input
+        type="range"
+        id="size"
+        min="1"
+        max="50"
+        value={size}
+        onChange={(e) => {
+          setSize(Number(e.target.value));
+        }}
+      />
+      <label htmlFor="size">Size</label>
+      <input
+        type="color"
+        id="color"
+        value={color}
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+      />
+      <label htmlFor="color">Color</label>
+      <Stage
+        width={500}
+        height={500}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+        style={{ display: 'inline-block', border: "solid", marginTop: "1rem" }}
+      >
+        <Layer>
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke={line.color}
+              strokeWidth={line.size}
+              tension={0.5}
+              lineCap="round"
+              globalCompositeOperation={line.tool === "eraser" ? "destination-out" : "source-over"}
+            />
+          ))}
+        </Layer>
+      </Stage>
     </div>
   );
-}
+};
 
 export default App;
